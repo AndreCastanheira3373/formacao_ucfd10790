98bfa85ec7ee4b1363a445bc4ce902963688b61b
AndreCastanheira3373 <115872123+AndreCastanheira3373@users.noreply.github.com>
2022-11-05 11:14:08 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import {useState, useEffect} from 'react';
 import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs";
 
-const API = "http://localhost:5000";
+//const API = "http://localhost:5000";
+const API = "https://formacaoucfd10790-5ob1j6ang-andrecastanheira3373.vercel.app:5000";
 
 function App() {
   const [title,setTitle] = useState("");
